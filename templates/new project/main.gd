eb63d1c39aac6802c7e0e0d221b5230e5723537f
Drillur <1992avery@gmail.com>
2025-08-12 13:52:37 -0600

    new project template

---
+++ templates/new project/main.gd
@@ -0,0 +1,10 @@
+extends Node
+
+
+# Called when the mod is loaded into LORED for the first time.
+func _ready() -> void:
+	
+	# To access LORED's mod helper functions, access the Kit script like this:
+	#get_node("/root/Kit")
+	
+	pass
