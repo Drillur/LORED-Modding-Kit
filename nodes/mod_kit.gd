5338b4958e3783ed8a7d261316ea8fd65efa4c7f
Drillur <1992avery@gmail.com>
2025-08-14 19:56:31 -0600

    almost ready for currencies

--- nodes/mod_kit.gd
+++ nodes/mod_kit.gd
@@ -9,97 +9,6 @@ const PATH: String = "res://groups/mod/LORED-Modding-Kit/"
 
 var signals := SignalBus.new()
 
-## In LORED, this holds any scripts added by mods
-## (except for main.gd, which is cached elsewhere when the mod is loaded)
-var cached_scripts: Dictionary[StringName, Dictionary]
-
-
-#region Ready
-
-
-func _ready() -> void:
-	if Engine.is_editor_hint():
-		_create_info_json()
-		_create_lored_data_json()
-
-
-#endregion
-
-
-#region Utility
-
-
-func _create_info_json() -> void:
-	const BASE_DATA: Dictionary = {
-		"key": "unique_identifier",
-		"author": "your_username",
-		"display name": "ModName",
-		"description": "short_summary_of_mods_effects",
-		"color": "#hex_color_for_your_mod",
-		"affects save": false,
-	}
-	
-	var info_json_path: String = PATH.path_join("templates/info.json")
-	if ResourceLoader.exists(info_json_path):
-		return
-	
-	var file := FileAccess.open(info_json_path, FileAccess.WRITE)
-	file.store_line(JSON.stringify(BASE_DATA, "\t", false))
-	#print(" - Bbcode must not be used in this file.")
-	#print(" - key: The mod's unique identifier. If it conflicts with another mod, it may be overwritten. LORED prepends the author text before the key to help make it more unlikely.")
-	#print(" - author: The mod's author.")
-	#print(" - display name: The name the players will see in-game.")
-	#print(" - description: A short summary of the mod's effects.")
-	#print(" - color: Various UI elements will use this color.")
-	#print(" - affects save: Set to true if the mod is expected to affect the save in any way.")
-
-
-func _create_lored_data_json() -> void:
-	const BASE_DATA: Dictionary = {
-		"Key": "iron",
-		"Title": "Iron",
-		"Name": "Will",
-		"Icon": "iron",
-		"Color": "#12e3ff",
-		"Description": "Wants everyone to succeed.",
-		"Favorite Thing": "Pizza parties!",
-		"Class": "LORED",
-		"Stage": "main1",
-		"Price": "9 stone, 8 copper",
-		"Price Increase": 3.0,
-		"Maximum Expected Level": 1000,
-		"Jobs": "iron",
-		"Primary Jobs": "iron",
-		"Primary Currencies": "iron",
-		"Autobuyer Level": 20,
-	}
-	
-	var lored_template_path: String = PATH.path_join("templates/lored_template.json")
-	if ResourceLoader.exists(lored_template_path):
-		return
-	
-	var file := FileAccess.open(lored_template_path, FileAccess.WRITE)
-	file.store_line(JSON.stringify(BASE_DATA, "\t", false))
-	#print(" - Key: The LORED's unique identifier.")
-	#print(" - Title: The LORED's official job title.")
-	#print(" - Name: The LORED's actual name.")
-	#print(" - Icon: Use the filename of the icon you want to use. Do not include the extension or path.")
-	#print(" - Color: The hex code for the LORED's color.")
-	#print(" - Descrption: Optional - some flavor text")
-	#print(" - Favorite Thing: Optional - more flavor text")
-	#print(" - Class: You may create your own class which extends LORED to add custom class behavior.")
-	#print(" - Stage: The key of the Stage that the LORED belongs to. If you add a custom Stage, set the key to that Stage's key.")
-	#print(" - Price: The base price for leveling up the LORED.")
-	#print(" - Price Increase: The amount the LORED's price increases by per level.")
-	#print(" - Maximum Expected Level: This determines how high LORED will cache its prices for quick purchasing.")
-	#print(" - Jobs: The starting job list for the LORED. If you want multiple, separate by a comma and a space. Example: growth, growth2")
-	#print(" - Primary Jobs: The job which affects currency rates. Growth only has 'growth' for this field. growth2 is not counted for rates.")
-	#print(" - Primary Currencies: The currencies which are displayed on the LORED HUD. If there are multiple, they will be cycled every 3 seconds.")
-	#print(" - Autobuyer Level: The level required to unlock the LORED's autobuyer.")
-
-
-#endregion
-
 
 #region Control
 
@@ -113,6 +22,69 @@ func emit_mods_loaded() -> void:
 
 func await_mods_loaded() -> void:
 	await signals.mods_loaded
+
+
+#endregion
+
+
+#region Currency
+
+
+func currency_add_amount(currency_key: StringName, amount: Variant) -> void:
+	Currency.add(currency_key, amount)
+
+
+func reset_currencies() -> void:
+	for currency: Currency in Currency.list.values():
+		currency.reset(10)
+
+
+#endregion
+
+
+#region LOREDs
+
+
+func add_job(job_key: StringName, json_path: String) -> void:
+	print("add_job(%s, %s)" % [job_key, json_path])
+	var file := FileAccess.open(json_path, FileAccess.READ)
+	if not file:
+		print(" - FileAccess.open failed")
+		return
+	var json_text := file.get_as_text()
+	var json := JSON.new()
+	json.parse(json_text)
+	
+	Job.data[job_key] = json.data
+	Job.new(job_key)
+
+
+func add_lored(lored_key: StringName, json_path: String) -> void:
+	print("add_lored()")
+	var file := FileAccess.open(json_path, FileAccess.READ)
+	if not file:
+		print(" - FileAccess.open failed")
+		return
+	var json_text := file.get_as_text()
+	var json := JSON.new()
+	json.parse(json_text)
+	
+	LORED.data[lored_key] = json.data
+	LORED.new(lored_key)
+
+
+func kill_loreds(loreds_to_kill: Array[StringName] = []) -> void:
+	if loreds_to_kill.is_empty():
+		for lored_key: StringName in LORED.list.keys():
+			loreds_to_kill.append(lored_key)
+	for lored_key: StringName in loreds_to_kill:
+		LORED.fetch(lored_key).kill()
+	LORED.signals.lored_killed.emit()
+
+
+func reset_loreds() -> void:
+	for lored: LORED in LORED.list.values():
+		lored.reset()
 
 
 #endregion
@@ -148,33 +120,22 @@ func refresh_stages() -> void:
 	Stage.signals.stages_changed.emit()
 
 
+func reset_stages() -> void:
+	for stage: Stage in Stage.list.values():
+		stage.reset()
+
+
 #endregion
 
 
-#region LOREDs
+#region Upgrades
 
 
-func add_lored(lored_key: StringName, json_path: String) -> void:
-	print("add_lored()")
-	var file := FileAccess.open(json_path, FileAccess.READ)
-	if not file:
-		print("FileAccess.open failed")
-		return
-	var json_text := file.get_as_text()
-	var json := JSON.new()
-	json.parse(json_text)
-	
-	LORED.data[lored_key] = json.data
-	LORED.new(lored_key)
-
-
-func kill_loreds(loreds_to_kill: Array[StringName] = []) -> void:
-	if loreds_to_kill.is_empty():
-		for lored_key: StringName in LORED.list.keys():
-			loreds_to_kill.append(lored_key)
-	for lored_key: StringName in loreds_to_kill:
-		LORED.fetch(lored_key).kill()
-	LORED.signals.lored_killed.emit()
+func reset_upgrades() -> void:
+	for upgrade: Upgrade in Upgrade.list.values():
+		if upgrade.key == &"unlock_upgrades":
+			continue
+		upgrade.reset(10)
 
 
 #endregion
--- templates/mod/main.gd
+++ templates/mod/main.gd
@@ -1,9 +1,23 @@
 extends Node
 
 
+#region Your Mod
+
+
 ## Called when the mod is loaded into LORED for the first time
 func _ready() -> void:
+	get_node("/root/Kit").signals.mods_loaded.connect(_on_mods_loaded)
+
+
+## Called when all mods finish loading in. This is where you should write your code!
+func _on_mods_loaded() -> void:
 	pass
+
+
+#endregion
+
+
+
 
 
 #region Kit helper functions
@@ -18,20 +32,20 @@ func _ready() -> void:
 #region Examples
 
 
-func remove_everything() -> void:
+func kill_all() -> void:
 	kill_loreds()
 	kill_stages()
 
 
 ## Unchanged, this won't work if you call it
 func add_custom_stages_and_new_loreds() -> void:
-	remove_everything()
+	kill_all()
 	
 	add_stage(&"new_stage_key", "path_to_stage.json")
 	
+	add_job(&"custom_job", "res://mod_name/loreds/jobs/custom_job.json")
+	
 	add_lored(&"schlonky", "res://mod_name/loreds/schlonky_data.json")
-	add_lored(&"wonky", "res://mod_name/loreds/wonky_data.json")
-	add_lored(&"fronky", "res://mod_name/loreds/fronky_data.json")
 	add_lored(&"scronky", "res://mod_name/loreds/scronky_data.json")
 	
 	refresh_stages()
@@ -43,14 +57,71 @@ func add_custom_stages_and_new_loreds() -> void:
 #region Control
 
 
+func reset_all() -> void:
+	reset_upgrades()
+	reset_currencies()
+	reset_stages()
+	reset_loreds()
+
+
 #region Signals
 
 
 ## When you write `await await_mods_loaded()`, your code will pause until
-## `Kit.signals.mods_loaded` is emitted. That will happen after this and every
-## other mod is loaded.
+## `Kit.signals.mods_loaded` is emitted. That will happen after every mod is loaded.
 func await_mods_loaded() -> void:
 	await get_node("/root/Kit").signals.mods_loaded
+
+
+#endregion
+
+
+#region Currency
+
+
+## Adds `amount` to a specified currency. `amount` can be an int or float,
+## or a string in the format of "'mantissa'e'exponent'", e.g. "1e6" or "5.5e20"
+func currency_add_amount(currency_key: StringName, amount: Variant) -> void:
+	get_node("/root/Kit").currency_add_amount(currency_key, amount)
+
+
+## Does not *kill* currencies; only resets them all to their `starting_amount`
+func reset_currencies() -> void:
+	get_node("/root/Kit").reset_currencies()
+
+
+#endregion
+
+
+#region LOREDs
+
+
+## Creates a Job using a .json file and stores it in memory for any LORED to use
+## Jobs **must** be added before adding LOREDs.
+func add_job(job_key: StringName, json_path: String) -> void:
+	get_node("/root/Kit").add_job(job_key, json_path)
+
+
+## Creates a new LORED using a .json file and stores them in memory. This will
+## NOT create a LOREDPrefab. You must create a Stage scene and place LORED
+## placeholder nodes in them. Refer to stage_templace.tscn for help :D
+func add_lored(lored_key: StringName, json_path: String) -> void:
+	get_node("/root/Kit").add_lored(lored_key, json_path)
+
+
+## Stops specified LOREDs from working, removes their prefabs, and deletes them
+## from memory. If you are replacing the old LOREDs, call this before adding new
+## ones. Murdered LOREDs cannot be resurrected.
+## NOTE - If `loreds_to_kill` is empty, it will kill every LORED, including
+## ones added from other mods.
+# idk if this actually matters ## NOTE - This should not be called before kill_stages().
+func kill_loreds(loreds_to_kill: Array[StringName] = []) -> void:
+	get_node("/root/Kit").kill_loreds(loreds_to_kill)
+
+
+## Resets all of the current LOREDs in memory to level 1.
+func reset_loreds() -> void:
+	get_node("/root/Kit").reset_loreds()
 
 
 #endregion
@@ -76,27 +147,21 @@ func refresh_stages() -> void:
 	get_node("/root/Kit").refresh_stages()
 
 
+## Resets the statistics of every Stage in memory
+func reset_stages() -> void:
+	get_node("/root/Kit").reset_stages()
+
+
 #endregion
 
 
-#region LOREDs
+#region Upgrades
 
 
-## Creates a new LORED using a .json file and stores them in memory. This will
-## NOT create a LOREDPrefab. You must create a Stage scene and place LORED
-## placeholder nodes in them. Refer to stage_templace.tscn for help :D
-func add_lored(lored_key: StringName, json_path: String) -> void:
-	get_node("/root/Kit").add_lored(lored_key, json_path)
-
-
-## Stops specified LOREDs from working, removes their prefabs, and deletes them
-## from memory. If you are replacing the old LOREDs, call this before adding new
-## ones. Murdered LOREDs cannot be resurrected.
-## NOTE - If `loreds_to_kill` is empty, it will kill every LORED, including
-## ones added from other mods.
-# idk if this actually matters ## NOTE - This should not be called before kill_stages().
-func kill_loreds(loreds_to_kill: Array[StringName] = []) -> void:
-	get_node("/root/Kit").kill_loreds(loreds_to_kill)
+## Sets all Upgrades in memory to unpurchased
+## (except `unlock_upgrades` which unlocks the Upgrades window)
+func reset_upgrades() -> void:
+	get_node("/root/Kit").reset_upgrades()
 
 
 #endregion
